f509eb79b2a349015be0c65133f81ac4cdddbb71
2051063936-3011 <nbtung772@gmail.com>
2024-01-17 17:51:53 +0700

    add

--- app/src/main/java/com/example/appquanly/TrangChu.java
+++ app/src/main/java/com/example/appquanly/TrangChu.java
@@ -3,24 +3,32 @@ package com.example.appquanly;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.appquanly.QLKH.qlkh;
 import com.example.appquanly.database.Databse_QuanLy;
 import com.google.android.material.navigation.NavigationView;
 
-public class TrangChu extends AppCompatActivity {
+public class TrangChu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
     TextView tenCH, bestseller;
+    FragmentManager fragmentManager;
     private Databse_QuanLy Database_QuanLy;
 
     @Override
@@ -30,7 +38,26 @@ public class TrangChu extends AppCompatActivity {
         Database_QuanLy = new Databse_QuanLy(this);
         AnhXa();
         actionBar();
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.mo, R.string.dong){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentManager = getSupportFragmentManager();
         Intent intent = getIntent();
+
 
     }
 
@@ -42,6 +69,21 @@ public class TrangChu extends AppCompatActivity {
         bestseller = findViewById(R.id.bestseller);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.itkhachhang:
+                FragmentTransaction tranHienThiQLKH = fragmentManager.beginTransaction();
+                HIenThiQLKH hIenThiQLKH = new HIenThiQLKH();
+                tranHienThiQLKH.replace(R.id.content, hIenThiQLKH);
+                tranHienThiQLKH.commit();
+                ;
+                break;
+        }
+        return false;
+    }
+
     private void actionBar(){
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -50,13 +92,6 @@ public class TrangChu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 drawerLayout.openDrawer(GravityCompat.START);
-                toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(TrangChu.this, qlkh.class);
-                        startActivity(intent);
-                    }
-                });
             }
         });
     }
